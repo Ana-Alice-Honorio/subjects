c8c7ec90cb0d6804549d7b6ca87a5106ae27eb4d
Ana Alice Honorio <ana.alice.honorio@gmail.com>
2025-03-10 17:54:01 -0300

    install vuetify, vue i18n, componentes vazios

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,29 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Importação do MDI Icons
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+})
+
+//vue i18n
+import i18n from './locales'
+
 const app = createApp(App)
 
 app.use(router)
-
+app.use(vuetify)
+app.use(i18n)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home-container">
+    <section id="math"><MathSubject /></section>
+    <section id="port"><PortugueseSubject /></section>
+    <section id="history"><HistorySubject /></section>
+    <section id="geo"><GeographySubject /></section>
   </main>
 </template>
+
+<script setup lang="ts">
+import MathSubject from '../components/MathSubject.vue'
+import PortugueseSubject from '../components/PortugueseSubject.vue'
+import HistorySubject from '../components/HistorySubject.vue'
+import GeographySubject from '../components/GeographySubject.vue'
+</script>
+
+<style scoped>
+.home-container {
+  scroll-snap-type: y mandatory;
+  overflow-y: visible;
+  height: 100vh;
+}
+
+section {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  scroll-snap-align: start;
+}
+</style>
